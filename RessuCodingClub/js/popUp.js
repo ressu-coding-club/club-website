1ed2472771767842763865ba0d059edfceac8a0e
lolman420 <daniel.nikkar@gmail.com>
2022-09-06 19:18:41 +0300

    small styling fixes

--- RessuCodingClub/js/popUp.js
+++ RessuCodingClub/js/popUp.js
@@ -1,9 +1,38 @@
+// @ts-check
+
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
+const FinnishLanguage = document.querySelector("finnish-button");
+const EnglishLanguage = document.querySelector("english-button");
+
+// EnglishLanguage.addEventListener("click", () => {
+//   console.log("hi");
+//   EnglishLanguage.classList.add("active-language");
+//   FinnishLanguage.classList.remove("active-language");
+// });
+
+// FinnishLanguage.addEventListener("click", () => {
+//   FinnishLanguage.classList.add("active-language");
+//   EnglishLanguage.classList.remove("active-language");
+// });
+
+// EnglishLanguage.addEventListener("ontouch", () => {
+//   EnglishLanguage.classList.add("active-language");
+//   FinnishLanguage.classList.remove("active-language");
+// });
+
+// FinnishLanguage.addEventListener("ontouch", () => {
+//   FinnishLanguage.classList.add("active-language");
+//   EnglishLanguage.classList.remove("active-language");
+// });
 
 openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+  button.addEventListener("ontouch", () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
   });
@@ -16,8 +45,19 @@ overlay.addEventListener("click", () => {
   });
 });
 
+overlay.addEventListener("ontouch", () => {
+  const modals = document.querySelectorAll(".pop-up.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
 closeModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    const modal = button.closest(".pop-up");
+    closeModal(modal);
+  });
+  button.addEventListener("ontouch", () => {
     const modal = button.closest(".pop-up");
     closeModal(modal);
   });
