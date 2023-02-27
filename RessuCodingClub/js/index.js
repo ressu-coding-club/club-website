ef8655f8dda4ec0b0875f4564d581acb230a50ab
lolman420 <daniel.nikkar@gmail.com>
2023-02-28 00:12:43 +0200

    added new design

---
+++ RessuCodingClub/js/index.js
@@ -0,0 +1,7 @@
+// @ts-check
+// ts-check for finding some stupid bugs that js allows (like having number and string calculations)
+// Be aware that it also shows those ONLY typescript bugs! (those can be ignored)
+
+document.getElementById("project-button").onclick = () => {
+  location.href = "www.youtube.com";
+};
